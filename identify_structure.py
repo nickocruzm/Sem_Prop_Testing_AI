388cadf2f899464c6f69c3cda4a151c38d5ea2e7
Nicko Cruz M <nickocruzm@gmail.com>
2026-03-16 20:48:30 -0700

    split branches into elements

--- identify_structure.py
+++ identify_structure.py
@@ -14,10 +14,9 @@ results = {}
 def extract_structure(node, code_bytes, structure):
     if node.type == "function_definition":
         name_node = node.child_by_field_name("name")
-        if name_node:
-            structure["functions"].append(
-                code_bytes[name_node.start_byte:name_node.end_byte].decode()
-            )
+        if name_node and structure["function"] is None:
+            structure["function"] = code_bytes[name_node.start_byte:name_node.end_byte].decode()
+
 
     if node.type == "if_statement":
         cond = node.child_by_field_name("condition")
@@ -26,11 +25,11 @@ def extract_structure(node, code_bytes, structure):
                 code_bytes[cond.start_byte:cond.end_byte].decode()
             )
 
-    if node.type in ("for_statement", "while_statement"):
-        structure["loops"].append(node.type)
+    # if node.type in ("for_statement", "while_statement"):
+    #     structure["loops"].append(node.type)
 
-    if node.type == "return_statement":
-        structure["returns"] += 1
+    # if node.type == "return_statement":
+    #     structure["returns"] += 1
 
     for child in node.children:
         extract_structure(child, code_bytes, structure)
@@ -38,9 +37,8 @@ def extract_structure(node, code_bytes, structure):
 PY_LANGUAGE = Language(tree_sitter_python.language())
 parser = Parser(PY_LANGUAGE)
 
-#parser.language = tree_sitter_python.language()
 
-for filename in os.listdir(python_dir)[:5]:
+for filename in os.listdir(python_dir):
     if not filename.endswith(".py"): continue
     
     filepath = os.path.join(python_dir, filename)
@@ -48,16 +46,17 @@ for filename in os.listdir(python_dir)[:5]:
         code = f.read()
     
     structure = {
-        "functions": [],
-        "branches": [],
-        "loops": [],
-        "returns": 0
+        "function":None,
+        "branches":[]
     }
     
     tree = parser.parse(code)
     root = tree.root_node
     extract_structure(root, code, structure)
-    results[filename] = structure
+    formatted = {"function": structure["function"]}
+    for i, cond in enumerate(structure["branches"], start=1):
+        formatted[f"branch{i}"] = cond
+        results[filename] = formatted
 
 output_path = os.path.join(logs_dir, "structure.json")
 with open(output_path, "w") as out:
